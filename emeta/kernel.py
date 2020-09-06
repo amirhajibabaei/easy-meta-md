fe88c634153fa8df9ffc96d1d89de214130e30fc
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2020-09-06 17:39:54 +0900

    kernel on variables

--- emeta/kernel.py
+++ emeta/kernel.py
@@ -3,17 +3,31 @@ from emeta.variable import Variable
 
 
 class Kernel(Variable):
-    pass
 
-    def __call__(self, x1, x2):
+    def __init__(self, u, v):
+        self.u = u
+        self.v = v
+
+    @property
+    def args(self):
+        return (self.u, self.v)
+
+    def __call__(self, *args, **kwargs):
+        x1 = self.u(*args, **kwargs)
+        x2 = self.v(*args, **kwargs)
         dim = tuple(range(abs(x1.dim()-x2.dim()), max(x1.dim(), x2.dim())))
         return self.eval(x1, x2, dim=dim)
 
 
 class Gaussian(Kernel):
 
-    def __init__(self, scale):
+    def __init__(self, u, v, scale):
+        super().__init__(u, v)
         self.scale = scale
+
+    @property
+    def args(self):
+        return (*super().args, self.scale)
 
     def eval(self, x1, x2, dim):
         return (x1-x2).div(self.scale).pow(2).sum(dim=dim).neg().exp()
