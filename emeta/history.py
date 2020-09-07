81a7ee6a67c710ca0921fab690e12746eb52b106
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2020-09-07 18:20:55 +0900

    autosave history

--- emeta/history.py
+++ emeta/history.py
@@ -5,12 +5,21 @@ import torch
 
 class History(Variable):
 
-    def __init__(self, var):
+    def __init__(self, var, file='history.txt'):
         self.var = var
         self.args = (var,)
+        self.kwargs = dict(file=file)
         self.history = []
+        self.write(f'# {var}', 'w')
+
+    def write(self, msg, mode='a'):
+        if self.kwargs['file']:
+            with open(self.kwargs['file'], mode) as f:
+                f.write(f'{msg}\n')
 
     def eval(self, *args, append=True, **kwargs):
         if append:
-            self.history.append(self.var(*args, *kwargs).clone().detach())
+            t = self.var(*args, *kwargs).clone().detach()
+            self.history.append(t)
+            self.write(t.tolist())
         return torch.stack(self.history)
