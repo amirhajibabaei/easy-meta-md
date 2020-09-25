d2205261899a8b6089a3085880498f71018e5698
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2020-09-25 18:13:19 +0900

    history here

--- emeta/variable.py
+++ emeta/variable.py
@@ -285,3 +285,24 @@ class Par(Variable):
 
     def _dot(self, data):
         self.dot_data += data
+
+
+class History(Variable):
+
+    def __init__(self, var, file=None):
+        super().__init__(var, file=file)
+        self.var = var
+        self.file = file
+        self.history = []
+        self.write(f'# {var}', 'w')
+
+    def write(self, msg, mode='a'):
+        if self.file:
+            with open(self.file, mode) as f:
+                f.write(f'{msg}\n')
+
+    def eval(self, contex):
+        t = self.var(contex).clone().detach()
+        self.history.append(t)
+        self.write(t.tolist())
+        return torch.cat(self.history)
